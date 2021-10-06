18f6be3ca0b8e4d874ade0399f1cdea4d13b3725
decpsg <ada@computing.sg>
2021-10-06 16:12:53 +0800

    Create magic_with_three.py

---
+++ magic_with_three.py
@@ -0,0 +1,3 @@
+# Write a program which will generate a list of numbers from 1 to 100 inclusive. 
+# When the number is a multiple of 3 or contains 3, show the symbol '@', else show the actual number. 
+
