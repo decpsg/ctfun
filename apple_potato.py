fe355ad6ef5358af8aef60b0ebff64cc649eb271
Gi Soong Chee <gi.soongchee@dhs.sg>
2021-10-30 10:02:00 +0800

    new demand and supply task using ct

---
+++ apple_potato.py
@@ -0,0 +1,7 @@
+# https://www.youtube.com/watch?v=1PbZMudPP5E
+# Using computational thinking and coding, determine
+# - How many apples can a potato farmer receive for x potatoes?
+# - How many potatoes can an apple farmer receive for y apples?
+# - What is the price of each potato/apple? 
+
+# main
