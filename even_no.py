6afd87b2c2b41829586829971ff35ca1036c50f7
Gi Soong Chee <gi.soongchee@dhs.sg>
2021-10-30 09:56:00 +0800

    refactor for programming style and rectify bug
    table should not contain strings
    suggest more meaningful/python identifier names
    pep8 white space

--- even_no.py
+++ even_no.py
@@ -1,6 +1,19 @@
+# Version 2: Suggested improvements
+# Determine even numbers
+
+numbers = [2, 3, 4]
+evens = []
+for number in numbers:
+  if number % 2 == 0:
+    evens.append(number) 
+print(evens)
+    
+# Version 1
+'''
 lst = []
 table = ["2", "3"]
 for i in table:
   if i%2 == 0:
     lst.append(i)
 print(lst)
+'''
